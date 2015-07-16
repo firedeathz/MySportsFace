8795f1f908cbbfe1938af33c465b17d05d347e39
firedeathz <mikazzz_pro@hotmail.com>
2015-07-16 15:58:05 +0100

    Added a FAQ page and an About page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   root   'static_pages#home'
   get 	 'help'    						=> 'static_pages#help'
   get	 'about'						=> 'static_pages#about'
-  get	 'contacts'						=> 'static_pages#contacts'
   get	 'admin_board'					=> 'static_pages#admin_board'
   get	 'organizations'				=> 'organizations#index'
   get	 'signup'  						=> 'users#new'
