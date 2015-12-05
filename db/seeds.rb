01b65814f545ace094220ca4da9220065996c1b1
firedeathz <mikazzz_pro@hotmail.com>
2015-12-05 16:50:51 +0000

    removed user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-User.create!(name:  "Marcos Germano", email: "mikazzz_pro@hotmail.com", password: "HEARTless0", password_confirmation: "HEARTless0", admin: true)
+# User.create!(name:  "Marcos Germano", email: "mikazzz_pro@hotmail.com", password: "HEARTless0", password_confirmation: "HEARTless0", admin: true)
 
 #50.times do |n|
 #  name  = Faker::Name.name
